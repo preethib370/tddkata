e68e0f5d24140f736d8937b325fd0ac0a1679d0e
PreethiBalakrishnan <preethib370@gmail.com>
2021-09-27 14:24:36 +0530

    Logic Implementation

---
+++ src/main/java/com/tddkata/stringcalculator/StringCalculator.java
@@ -0,0 +1,33 @@
+package com.tddkata.stringcalculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringCalculator {
+	public int add(String input) {
+		int result = 0;
+		List<Integer> negativeNum = new ArrayList<Integer>();
+		String temp = input;
+		String delimiter = ",|\n";
+		if (temp.startsWith("//")) {
+			int index = temp.indexOf("//") + 2;
+			delimiter = temp.substring(index, index + 1);
+			input = temp.substring(temp.indexOf("\n") + 1);
+		}
+		String[] inputArray = input.split(delimiter);
+		for (String i : inputArray) {
+			if (!i.trim().isEmpty()) {
+				int number = Integer.parseInt(i.trim());
+				if (number < 0) {
+					negativeNum.add(number);
+				} else if (number <= 1000) {
+					result += number;
+				}
+			}
+		}
+		if (negativeNum.size() != 0) {
+			throw new RuntimeException("Negatives not allowed: " + negativeNum.toString());
+		}
+		return result;
+	}
+}
